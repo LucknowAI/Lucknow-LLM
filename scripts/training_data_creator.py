d8517fdd778a01efc3123a11704b02c87a19003c
Abhishek Verma <abhishekverm2018@gmail.com>
2024-03-08 00:55:37 +0530

    added method to clean blank lines changed creator method, now it takes absolute file path as input instead of string.

--- scripts/training_data_creator.py
+++ scripts/training_data_creator.py
@@ -1,8 +1,9 @@
 import textwrap
 import google.generativeai as genai
 from IPython.display import Markdown
+import time
 
-GOOGLE_API_KEY = "PASTE YOUR API KEY HERE"
+GOOGLE_API_KEY = "AIzaSyBE2A0E_MIpf-i9cZRSRXP430Br4FZxJqc"
 
 
 class TrainDataCreator:
@@ -12,7 +13,8 @@ class TrainDataCreator:
     def __init__(self):
         pass
 
-    def get_prompt(self):
+    @staticmethod
+    def get_prompt():
         with open('prompt.txt', 'r') as f:
             prompt_data = f.read()
         return prompt_data
@@ -22,20 +24,41 @@ class TrainDataCreator:
         text = text.replace('•', '  *')
         return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
-    def create(self, txt: str):
+    def create(self, file, new_file_name='train_data.json'):
         """Provides instances in gemini format.
 
-        This function takes paragraph(str) as input and provide
-        pairs of question and answer in gemini format.
-        Pairs can be more than one, so use accordingly.
-        :param txt: string
+        This function takes text file as input and provide
+        output in formate of pairs of question and answer.
+        :param file: absolute path of file
         :return lines: list of JSON objects."""
 
         prompt = self.get_prompt()
-        full_prompt = prompt + txt + "\n\nOutput: \n"
-        gemini_response = self.model.generate_content(full_prompt)
-        try:
-            response_ = eval(gemini_response.text)
-            return response_
-        except Exception as e:
-            return "Response format Error"
+        new_train_file = open(new_file_name, 'w')
+        with open(file) as file:
+            lines = file.readlines()
+            for line in lines:
+                full_prompt = prompt + line + "\n\nOutput: \n"
+                gemini_response = self.model.generate_content(full_prompt)
+                try:
+                    response_ = eval(gemini_response.text)
+                    print(gemini_response.text)
+                    new_train_file.writelines(gemini_response.text)
+                except Exception as e:
+                    print( "Response format Error")
+
+    def clean(self, file):
+        clean_file = open('clean_file.txt', 'w')
+        with open(file, 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                if len(line) < 5:
+                    pass
+                else:
+                    clean_file.write(line)
+        print("File have no blank line now!")
+
+creator = TrainDataCreator()
+# path = "/home/thor_x_me/PycharmProjects/Lucknow-LLM-data/Unstructured_data/Famous_Personalities/Famous_personalities.txt"
+# creator.clean(path)
+clean_path = "/home/thor_x_me/PycharmProjects/Lucknow-LLM-data/scripts/clean_file.txt"
+creator.create(clean_path)
