54eb9b0bb2baa1db308d714f38ac922540217e22
Monk (looking for PhD Fall’24) <aadityaura@gmail.com>
2024-03-10 23:08:19 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lucknowllm',
+    version='0.1',
+    packages=find_packages(),
+    description='llm for lucknow',
+    long_description=open('README.md').read(),
+    author='Lucknow AI Labs Team',  # Your name
+    url='https://github.com/LucknowAI/Lucknow-LLM',  # URL to your package's repository
+)
