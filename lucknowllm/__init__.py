e6aafb18e88da033114825c7004ba287ec92afed
Monk (looking for PhD Fall’24) <aadityaura@gmail.com>
2024-03-10 22:43:07 +0530

    Update __init__.py

--- lucknowllm/__init__.py
+++ lucknowllm/__init__.py
@@ -1,3 +1,5 @@
 from .models.gemini_model import *
 from .preprocessing.raw_data_preprocessing import *
 from .prompts.prompts import *
+from .data.unstructured_data import *
+from .data.structured_data import *
